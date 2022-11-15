f6c129aaa323eb962f0bab1f4f709c746fee698a
Kieran Knowles <kieranknowles11@hotmail.co.uk>
2022-11-15 14:14:09 +0000

    Format StudentSimulation.java

--- src/genethoc/StudentSimulation.java
+++ src/genethoc/StudentSimulation.java
@@ -9,50 +9,43 @@ package genethoc;
  *
  * @author nickdalton
  */
-public class StudentSimulation  extends GenHocDNADatabase
-{ 
-    /**
-     * Over ride methods of GenHocDNADatabase to speed them up. 
-     *  You could override the DNASequence as well like this. 
-     */
-    
-    
-    static  class StudentDNASqeuence extends DNASequence
-    { 
+public class StudentSimulation extends GenHocDNADatabase {
 
-        public StudentDNASqeuence(String dnaSequence, String sampleID) 
-        {
+    /**
+     * Over ride methods of GenHocDNADatabase to speed them up. You could
+     * override the DNASequence as well like this.
+     */
+
+    static class StudentDNASqeuence extends DNASequence {
+
+        public StudentDNASqeuence(String dnaSequence, String sampleID) {
             super(dnaSequence, sampleID);
         }
     }
+
     //=================== END OF CLASS ===========================
-    public DNASequence makeRandomDNASequence(int maxCrimeScens  )
-    { 
-    
-    DNASequence it = new StudentDNASqeuence(
-                  DNASequenceInterface.generateRandomDNA( this.dice ), ""+ 
-                                            (++GenHocDNADatabase.idCode));
-       it.addRandomCrimeLocations(it, this.dice,maxCrimeScens );
-       return it ; 
-        
+    public DNASequence makeRandomDNASequence(int maxCrimeScens) {
+
+        DNASequence it = new StudentDNASqeuence(
+                DNASequenceInterface.generateRandomDNA(this.dice), ""
+                + (++GenHocDNADatabase.idCode));
+        it.addRandomCrimeLocations(it, this.dice, maxCrimeScens);
+        return it;
+
     }
-         
-    /** 
-     * for assessment purposes don't change 
-     * @param seed 
+
+    /**
+     * for assessment purposes don't change
+     *
+     * @param seed
      */
-    public StudentSimulation(long seed) 
-    {
+    public StudentSimulation(long seed) {
         super(seed);
     }
-    
-    public static void main(String[] args) 
-    {
-       StudentSimulation db = new StudentSimulation( System.currentTimeMillis() ) ; 
-       db.everythingWhichShouldBeImproved(); 
+
+    public static void main(String[] args) {
+        StudentSimulation db = new StudentSimulation(System.currentTimeMillis());
+        db.everythingWhichShouldBeImproved();
     }
-    
-    
-   
-    
+
 }
